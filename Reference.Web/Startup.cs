14dbcdf2890a7ebdf4ff675093035ef8c8334217
Jaime Still <jpstill85@gmail.com>
2016-02-17 19:36:52 -0500

    Updated Startup configuration

--- Reference.Web/Startup.cs
+++ Reference.Web/Startup.cs
@@ -2,16 +2,19 @@
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
+using Reference.Data;
+using Reference.Web.Infrastructure;
 
 [assembly: OwinStartup(typeof(Reference.Web.Startup))]
 
 namespace Reference.Web
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            app.CreatePerOwinContext(AppDbContext.Create);
+            app.CreatePerOwinContext<UserManager>(UserManager.Create);
         }
     }
 }
